c0a147c74b93be6ad72674eba5bf2d679b1819e4
hiroyuki <hy2_710788347w@icloud.com>
2023-10-27 22:04:31 +0900

    パスワード入力前に印を表示

--- PasswordMake.java
+++ PasswordMake.java
@@ -2,35 +2,36 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {	  
-  public static void main(String[] args) {
-	  String Password ;
-	  Scanner scanner = new Scanner(System.in);
-      System.out.println("パスワードの桁数を入力してください。");
-      int num = scanner.nextInt();
-      Password = makePassord(num);
-      System.out.println(Password);
-      scanner.close();
-      System.out.println("完了");
-  }
+	public static void main(String[] args) {
+		String Password ;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("パスワードの桁数を入力してください。");
+		System.out.print(":");
+		int num = scanner.nextInt();
+		Password = makePassord(num);
+		System.out.println(Password);
+		scanner.close();
+		System.out.println("完了");
+	}
 
-private static String makePassord(int num) {
-	  String keyWord[] = new String[4];{
-		  keyWord[0] = "abcdefghijklmnopqrstuvwxyz";
-		  keyWord[1] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		  keyWord[2] = "1234567890";
-		  keyWord[3] = "!#$%&()@<>?_";
-		  }
-      Random rand = new Random();
-      int num2 = num;
-      String newPassword = "";
+	private static String makePassord(int num) {
+		String keyWord[] = new String[4];{
+			keyWord[0] = "abcdefghijklmnopqrstuvwxyz";
+			keyWord[1] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			keyWord[2] = "1234567890";
+			keyWord[3] = "!#$%&()@<>?_";
+		}
+		Random rand = new Random();
+		int num2 = num;
+		String newPassword = "";
 
-    for(int i = 0 ; i < num2 ; i++){
-      int ramNum  = rand.nextInt(4);
-      int ramNum2 = rand.nextInt(keyWord[ramNum].length());
-      String key = keyWord[ramNum].substring(ramNum2,(ramNum2 +1));
-      newPassword = newPassword + key;
-      }
-    return newPassword;
-  }
+		for(int i = 0 ; i < num2 ; i++){
+			int ramNum  = rand.nextInt(4);
+			int ramNum2 = rand.nextInt(keyWord[ramNum].length());
+			String key = keyWord[ramNum].substring(ramNum2,(ramNum2 +1));
+			newPassword = newPassword + key;
+		}
+		return newPassword;
+	}
 
 }
